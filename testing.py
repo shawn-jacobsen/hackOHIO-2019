1e9951fe169d96c13872dc19a325dbd0b75abf3d
Gary Sung <44556677+garysung0@users.noreply.github.com>
2019-11-02 17:51:34 -0400

    testing

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import mobikit
 
 mobikit.set_api_key(373afebc8a2b2ee5d5e7743cc73565c82d9ecdd8)
-
+# workspaceid = 168
+# fee id = 265
 dataframes = mobikit.workspaces.load(<work_space_id::int>, <feed_name::str?>, <query::object?>)
